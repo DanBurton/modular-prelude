543c83ba23a198d11e2db7660c5ee8d44fbd5ced
Dan Burton <danburton.email@gmail.com>
2012-08-11 17:41:43 -0600

    Depend on basic-prelude and data-default

--- ModularPrelude.hs
+++ ModularPrelude.hs
@@ -2,8 +2,12 @@
 
 module ModularPrelude
     ( module X
+    , LHashMap
     ) where
 
 import BasicPrelude as X
-import ModularPrelude.Default as X
+import Data.Default as X
+
+import qualified Data.HashMap.Lazy
+type LHashMap = Data.HashMap.Lazy.HashMap
 
